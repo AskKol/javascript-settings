db9c68c40bb3a7e6a932cf221fdea28a1455f96f
admin <admin@TeamCity>
2018-06-01 11:53:47 +0100

    TeamCity change in 'JavaScript' project: runners of '03. Deploy To Staging' build configuration were updated

--- .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
@@ -17,7 +17,7 @@ object JavaScript_03DeployToStaging : BuildType({
     steps {
         script {
             name = "IIS Deploy"
-            scriptContent = """rmdir /S /Q \inetpub\wwwroot xcopy /S /Y app \inetpub\wwwroot\"""
+            scriptContent = """ROBOCOPY .\app C:\inetpub\wwwroot\javascriptcards\ *.* /E /MIR /NP IF %%ERRORLEVEL%% LEQ 3 set errorlevel=0 IF %%ERRORLEVEL%% LEQ 0 EXIT /b %%ERRORLEVEL%% EXIT 0"""
         }
     }
 
