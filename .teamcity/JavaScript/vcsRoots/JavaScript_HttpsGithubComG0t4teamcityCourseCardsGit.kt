2ca2866abb24cfdee3d9abc2cc475111777f8465
admin <admin@TeamCity>
2018-06-01 09:39:09 +0100

    TeamCity change in 'JavaScript' project: 'https://github.com/g0t4/teamcity-course-cards.git' VCS root was updated

--- .teamcity/JavaScript/vcsRoots/JavaScript_HttpsGithubComG0t4teamcityCourseCardsGit.kt
+++ .teamcity/JavaScript/vcsRoots/JavaScript_HttpsGithubComG0t4teamcityCourseCardsGit.kt
@@ -6,6 +6,10 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.vcs.GitVcsRoot
 object JavaScript_HttpsGithubComG0t4teamcityCourseCardsGit : GitVcsRoot({
     uuid = "e3410fd4-7167-404d-b41f-9b71a7a78085"
     id = "JavaScript_HttpsGithubComG0t4teamcityCourseCardsGit"
-    name = "https://github.com/g0t4/teamcity-course-cards.git"
-    url = "https://github.com/g0t4/teamcity-course-cards.git"
+    name = "https://github.com/AskKol/teamcity-course-cards.git"
+    url = "https://github.com/AskKol/teamcity-course-cards.git"
+    authMethod = password {
+        userName = "Askkol"
+        password = "credentialsJSON:e534b11e-d6db-4b89-922a-0fd371f3a6d1"
+    }
 })
